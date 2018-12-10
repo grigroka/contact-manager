d851cab6dfa56c07b892a2c43f02974a5125c2ba
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-10 15:37:07 +0200

    fixed UI

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="333-333-3333"
-        />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
