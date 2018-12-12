62f97ffc08d31f398b1af7b88bfe3d872be497e1
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-12 11:33:14 +0200

    Updates

---
+++ precache-manifest.ff9299799d1732995885323c9ed11726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "8a1080caddea94a886e3",
+    "url": "/contact-manager/static/js/main.8a1080ca.chunk.js"
+  },
+  {
+    "revision": "bda6a27d174290dcde2c",
+    "url": "/contact-manager/static/js/1.bda6a27d.chunk.js"
+  },
+  {
+    "revision": "8a1080caddea94a886e3",
+    "url": "/contact-manager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "bda6a27d174290dcde2c",
+    "url": "/contact-manager/static/css/1.cef084f1.chunk.css"
+  },
+  {
+    "revision": "77c56afaa2f13090466d45565229a9e8",
+    "url": "/contact-manager/index.html"
+  }
+];
