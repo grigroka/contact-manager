30e063c99b50a7ee3864c17efa7b130a2560eab3
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-12 11:15:12 +0200

    EditContact component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import Test from './components/test/Test';
 
 import { Provider } from './context';
@@ -23,6 +24,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
