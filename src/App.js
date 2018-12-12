a39220253da9b23f0111a55f05b56955ad291418
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-12 10:04:15 +0200

    Not Found page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/layout/Header';
-import About from './components/pages/about';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
